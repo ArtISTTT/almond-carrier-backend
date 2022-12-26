daf7be38407ee723ea4d839bae003c19cac967fc
agazukin <agazukin@velixo.com>
2022-12-26 12:32:41 +0800

    only origin route

--- app/routes/auth.routes.ts
+++ app/routes/auth.routes.ts
@@ -5,10 +5,10 @@ import * as controller from '../controllers/auth.controller';
 
 export default (app: Express) => {
     app.use((req, res, next) => {
-        // res.setHeader('Access-Control-Allow-Origin', [
-        //     'https://friendlycarrier.com',
-        //     'http://localhost:3000',
-        // ]);
+        res.setHeader(
+            'Access-Control-Allow-Origin',
+            'https://friendlycarrier.com'
+        );
         res.header(
             'Access-Control-Allow-Headers',
             'Origin, Content-Type, Accept'
