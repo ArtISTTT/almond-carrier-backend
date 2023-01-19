2117456578cefb01110e8f782acc771306cece31
agazukin <agazukin@velixo.com>
2023-01-19 23:54:06 +0800

    remove logs

--- app/socketio/index.ts
+++ app/socketio/index.ts
@@ -3,12 +3,9 @@ import { Client } from 'socket.io/dist/client';
 
 class WebSockets {
     connection(socket: Socket) {
-        console.log('[SOCKET.IO] Connected new user');
-
         socket.emit('connected');
 
         socket.on('connect-to-order', (orderId: string) => {
-            console.log('[SOCKET.IO] connected to order');
             socket.join(orderId);
         });
     }
