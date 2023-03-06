527fdd9341f2e1b9ea6332b64b545a9fdae7b4bf
agazukin <agazukin@velixo.com>
2023-03-06 12:52:57 +0800

    fix

--- app/mailService/verificationCode.ts
+++ app/mailService/verificationCode.ts
@@ -1,19 +1,23 @@
 import { transporter } from './transporter';
 
 export const sendVerification = (link: string, email: string) => {
-    transporter.sendMail(
-        {
-            from: 'support@friendlycarrier.com',
-            to: email,
-            subject: 'Account verification code',
-            text: `Your account verification code: ${link}`,
-        },
-        (err, info) => {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log(info);
+    try {
+        transporter.sendMail(
+            {
+                from: 'support@friendlycarrier.com',
+                to: email,
+                subject: 'Account verification code',
+                text: `Your account verification code: ${link}`,
+            },
+            (err, info) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(info);
+                }
             }
-        }
-    );
+        );
+    } catch (e) {
+        console.log(e);
+    }
 };
