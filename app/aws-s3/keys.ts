e8467ca6d7e2d840079831eb0a3d6286749039f2
agazukin <agazukin@velixo.com>
2023-03-22 11:42:31 +0800

    gitignore

--- app/aws-s3/keys.ts
+++ app/aws-s3/keys.ts
@@ -1,4 +1,4 @@
 export const keys = {
-    iam_access_id: 'AKIA6PEZ2BDX53WRVMC5',
-    iam_secret: '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',
+    iam_access_id: process.env.IAM_ACCESS_ID,
+    iam_secret: process.env.hM1PeovkqgXoBETiXIOzsEhg803WLdn4vk2loEu7,
 };
