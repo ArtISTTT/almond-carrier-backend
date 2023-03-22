67f3215e420068f0265eaaf2c8201a9e81566774
agazukin <agazukin@velixo.com>
2023-03-22 11:42:55 +0800

    fix secret

--- app/aws-s3/keys.ts
+++ app/aws-s3/keys.ts
@@ -1,4 +1,4 @@
 export const keys = {
     iam_access_id: process.env.IAM_ACCESS_ID,
-    iam_secret: process.env.hM1PeovkqgXoBETiXIOzsEhg803WLdn4vk2loEu7,
+    iam_secret: process.env.IAM_SECRET,
 };
