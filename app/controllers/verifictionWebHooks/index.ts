df67e6a1a5af07fe1fc6fcefd81be64fdc1deca4
agazukin <agazukin@velixo.com>
2023-04-19 20:59:16 +0800

    fix

--- app/controllers/verifictionWebHooks/index.ts
+++ app/controllers/verifictionWebHooks/index.ts
@@ -6,6 +6,8 @@ const User = db.user;
 export const verificationWebHook = async (req: Request, res: Response) => {
     const verificationResult = req;
 
+    console.log(req);
+
     // await User.findOneAndUpdate(
     //     {
     //         email: verificationResult.email,
