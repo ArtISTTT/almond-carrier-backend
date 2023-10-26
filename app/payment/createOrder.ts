d3dcb7e5688ac251b177548672fecb026b5e890c
ArtISTTT <artemis09035@gmail.com>
2023-10-26 20:37:59 +0300

    fix

--- app/payment/createOrder.ts
+++ app/payment/createOrder.ts
@@ -3,13 +3,6 @@ import { encodeBase64 } from 'bcryptjs';
 import md5 from 'md5';
 import { generateHMACSignature } from '../helpers/generateHMACSignature';
 
-const instance = axios.create({
-    baseURL: process.env.PAYGINE_URI,
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-    },
-});
-
 interface ICompleteOrderForPayment {
     txnId: string;
 }
@@ -65,38 +58,29 @@ export const createOrderForPayout = async ({
     productName,
     sdRef,
 }: ICreateOrderForPayout): Promise<string | undefined> => {
-    const currency = 643;
-    const resAmount = amount * 100;
+    // const currency = 643;
+    // const resAmount = amount * 100;
 
-    const sector = process.env.PAYGINE_SECTOR_ID as string;
-    const password = process.env.PAYGINE_PASSWORD as string;
-    const signatureString =
-        sector + resAmount.toString() + currency.toString() + password;
-    const md5String = md5(signatureString, {
-        encoding: 'UTF-8',
-    });
-    const signature = Buffer.from(md5String).toString('base64');
-
-    try {
-        const data = await axios.post(
-            `${process.env.PAYGINE_URI}webapi/Register`,
-            undefined,
-            {
-                params: {
-                    signature,
-                    sector,
-                    amount: resAmount,
-                    reference: orderId,
-                    currency,
-                    description: productName,
-                    mode: 1,
-                    sd_ref: sdRef,
-                },
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                },
-            }
-        );
+    // try {
+    //     const data = await axios.post(
+    //         `${}webapi/Register`,
+    //         undefined,
+    //         {
+    //             params: {
+    //                 signature,
+    //                 sector,
+    //                 amount: resAmount,
+    //                 reference: orderId,
+    //                 currency,
+    //                 description: productName,
+    //                 mode: 1,
+    //                 sd_ref: sdRef,
+    //             },
+    //             headers: {
+    //                 'Content-Type': 'application/x-www-form-urlencoded',
+    //             },
+    //         }
+    //     );
 
         return data.data;
     } catch (e) {
